83af9f456b65339a26036ae808d5dccf60131ee0
hungnv2311 <hungnv2311@gmail.com>
2022-05-11 10:43:29 +0700

    feat: Add argparse

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 
 from tqdm import tqdm
@@ -23,35 +24,32 @@ def get_properties():
     with get_client() as client:
         projects = get_resource(client, "projects", "")
 
-        buildings = iterate(client, 'buildings', projects)
-        floors = iterate(client, 'floors', buildings)
-        properties = iterate(client, 'properties', floors)
+        buildings = iterate(client, "buildings", projects)
+        floors = iterate(client, "floors", buildings)
+        properties = iterate(client, "properties", floors)
 
     return properties
 
 
-def get_valuations():
-    with open("properties.json") as f:
+def get_valuations(file_: int):
+    with open(f"properties/properties.{file_}.json", "r") as f:
         properties = json.load(f)
 
-    size = 5000
+    with get_client() as client:
+        valuations = [
+            get_resource(client, "apartment-valuation", property_)
+            for property_ in tqdm(properties)
+        ]
 
-    properties_group = [properties[i: i + size] for i in range(0, len(properties), size)]
-
-    for i, group in enumerate(properties_group):
-        with open(f"properties/properties.{i}.json", 'w') as f:
-            json.dump(group, f)
-
-    # with get_client() as client:
-    #     valuations = [
-    #         get_resource(client, "apartment-valuation", property_)
-    #         for property_ in tqdm(properties)
-    #     ]
-
-    #     with open("data.json", "w") as f:
-    #         json.dump(valuations, f)
-
+        with open(f"valuations/valuations.{file_}.json", "w") as f:
+            json.dump(valuations, f)
 
 
 if __name__ == "__main__":
-    get_valuations()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("file", type=int)
+
+    args = parser.parse_args()
+
+    get_valuations(args.file)
